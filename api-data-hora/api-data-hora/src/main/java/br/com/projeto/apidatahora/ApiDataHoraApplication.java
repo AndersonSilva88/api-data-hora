eca35f8c3d94a839f6f32150039ae21f1d8357f7
perkles <anderson.os88@hotmail.com>
2021-10-29 19:31:34 -0300

    api data e hora

---
+++ api-data-hora/api-data-hora/src/main/java/br/com/projeto/apidatahora/ApiDataHoraApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.apidatahora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDataHoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDataHoraApplication.class, args);
+	}
+
+}
---
+++ api-data-hora/api-data-hora/src/main/java/br/com/projeto/apidatahora/controller/DataHoraController.java
@@ -0,0 +1,27 @@
+package br.com.projeto.apidatahora.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/data-hora") // http://localhost:8080/data-hora
+public class DataHoraController {
+
+    @GetMapping
+    public Map<String, Object> retornarDataHora(
+            @RequestParam(value = "timezone", defaultValue = "America/São_Paulo") String timezone
+    ){
+        var zone = ZoneId.of(timezone);
+        var datahora = LocalDateTime.ofInstant(Instant.now(), zone);
+        return Map.of("dataHora", datahora);
+
+    }
+
+}
